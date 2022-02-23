a8101d20c6f009d5e387bc727d56ad487cb955d4
chemaT <chemaaa94@gmail.com>
2022-02-22 21:14:09 -0600

    formula de Circulo

--- main.js
+++ main.js
@@ -18,12 +18,30 @@ console.group("Triangulo");
 const ladoTriangulo1 = 6;
 const ladoTriangulo2 = 6;
 const baseTriangulo = 4;
+const alturaTriangulo = 5.5;
 
 console.log (`los lados del triangullo miden ${ladoTriangulo1}cms ${ladoTriangulo2}cms ${baseTriangulo}cms`);
+console.log(`La altura del triangulo es de  ${alturaTriangulo} cms`);
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log(`El perimetro del triangulo es de ${perimetroTriangulo}`);
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log (`El area del triangulo es de ${areaTriangulo} cm^2`);
 
 console.groupEnd();
 
+ //CODIGO DEL CIRCULO
 console.group("Circulo");
 
+const radioCirculo = 4;
+const diametroCirculo = radioCirculo * 2;
+const PI = Math.PI;
+
+const perimetroCirculo = parseFloat(diametroCirculo * PI).toFixed(2);
+console.log(`El perimetro del circulo es ${perimetroCirculo} cm`)
+
+const areaCirculo = parseFloat((radioCirculo * radioCirculo) * PI).toFixed(2);
+console.log(`El area del circulo es de ${areaCirculo} cm^2`)
 
 console.groupEnd();
