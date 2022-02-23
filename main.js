9200b1d925f0d2cefa0c16426a74b1248a9698db
chemaT <chemaaa94@gmail.com>
2022-02-22 20:53:08 -0600

    formulas cuadrado y triangulo

---
+++ main.js
@@ -0,0 +1,29 @@
+//Codigo del Cuadrado
+console.group("Cuadrado");
+// Lado del Cuadrado
+const ladoCuadrado = 5;
+console.log(`los lados del cuadrado miden ${ladoCuadrado} cms`);
+//Perimetro del Cuadrado
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log(`el perimetro del cuadro es ${perimetroCuadrado} cms`);
+// Area del Cuadrado
+const areaCuadrada = ladoCuadrado * ladoCuadrado;
+console.log(`el area del cuadro es ${areaCuadrada} cms^2`);
+//--------------------------------------------------------------//
+console.groupEnd ();
+
+
+// Codigo del Triangulo
+console.group("Triangulo");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log (`los lados del triangullo miden ${ladoTriangulo1}cms ${ladoTriangulo2}cms ${baseTriangulo}cms`);
+
+console.groupEnd();
+
+console.group("Circulo");
+
+
+console.groupEnd();
