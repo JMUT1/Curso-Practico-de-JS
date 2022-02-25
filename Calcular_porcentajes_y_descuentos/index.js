0d48f16ca1d45e1f861471b400bc83712e3377cc
chemaT <chemaaa94@gmail.com>
2022-02-25 10:23:41 -0600

    Calcular calculadora precio con descuentos

---
+++ Calcular_porcentajes_y_descuentos/index.js
@@ -0,0 +1,8 @@
+
+function calcularPrecioConDescuento (precio, descuento){
+    
+    const porcentajeDelPrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajeDelPrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
