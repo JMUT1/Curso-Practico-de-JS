10a79e17084a29602ebf9a3bc8f0d23ff102abf1
chemaT <chemaaa94@gmail.com>
2022-02-25 10:54:57 -0600

    Interacion HMTL con JS de calculadora de Descuento

--- Calcular_porcentajes_y_descuentos/index.js
+++ Calcular_porcentajes_y_descuentos/index.js
@@ -6,3 +6,19 @@ function calcularPrecioConDescuento (precio, descuento){
 
     return precioConDescuento;
 }
+
+//Interacion HTML y JS
+
+function onClickButtonPriceDiscount () {
+    const precioUsuario = document.getElementById("inputPrice");
+    const precioValue = precioUsuario.value;
+
+    const discountUsuario = document.getElementById("inputDiscount");
+    const discountValue = discountUsuario.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(precioValue,discountValue);
+
+    const resultP = document.getElementById("ResultPrice");
+
+    resultP.innerText = `El precio con descuento son $${precioConDescuento} pesos`
+}
