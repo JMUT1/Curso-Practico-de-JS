d91257a316424bd888adf8dcc4c2f5b2c71e2428
chemaT <chemaaa94@gmail.com>
2022-02-26 13:05:13 -0600

    Avance de formulas de la Mediana

---
+++ Promedio_Moda_Mediana/Mediana/index.js
@@ -0,0 +1,16 @@
+const lista1 = [100,200,300,500,400000];
+
+const mitadlista1 = lista1.length / 2;
+
+function esPar(numerito){
+    if (numerito % 2 === 0 ){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+if (esPar(lista.length)){
+
+}
