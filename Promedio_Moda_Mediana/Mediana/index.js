300440eb9c42b9d19a558b803f9b2af0c171864a
chemaT <chemaaa94@gmail.com>
2022-02-26 13:24:09 -0600

    Formula es Mediana

--- Promedio_Moda_Mediana/Mediana/index.js
+++ Promedio_Moda_Mediana/Mediana/index.js
@@ -1,7 +1,19 @@
+function calcularMediaAritmetica(lista) {
+
+    const sumaLista = lista.reduce(
+        function(valorAcumulado = 0, nuevoElemento){
+            return valorAcumulado + nuevoElemento;
+        }
+    ); 
+      const promedio = sumaLista / lista.length;
+      return promedio;
+    }
+
 const lista1 = [100,200,300,500,400000];
 
-const mitadlista1 = lista1.length / 2;
+const mitadlista1 =  parseInt (lista1.length / 2);
 
+//Funciones validadora
 function esPar(numerito){
     if (numerito % 2 === 0 ){
         return true;
@@ -11,6 +23,16 @@ function esPar(numerito){
     }
 }
 
-if (esPar(lista.length)){
+let mediana;
 
+if (esPar(lista1.length)){
+const elemento1 = lista1[mitadlista1];
+const elemento2 = lista1 [mitadlista1 - 1];
+
+const promedioElemento1y2 = calcularMediaAritmetica([elemento1, elemento2,])
+
+mediana = promedioElemento1y2;
 }
+else {
+    mediana = lista1 [mitadlista1];
+};
